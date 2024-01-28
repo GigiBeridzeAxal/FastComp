31c9ed04aafd21694b13b361a53de5d35c4b6dde
GigiBeridzeAxal <145029127+GigiBeridzeAxal@users.noreply.github.com>
2024-01-28 17:06:43 +0400

    First Commit

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
 <>
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link rel="preconnect" href="https://fonts.gstatic.com" />
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Georgian:wght@600&display=swap" rel="stylesheet"/>
  <Header></Header>
   <Getstarted></Getstarted>
